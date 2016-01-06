7cd586ce6caae71b7410b213b944b6bc39baaa21
Steve <stevenlyall@gmail.com>
2016-01-06 13:46:43 -0800

    first upload

---
+++ src/ca/stevenlyall/BankAccountTester.java
@@ -0,0 +1,26 @@
+package ca.stevenlyall;
+
+
+public class BankAccountTester {
+
+    public static void main(String[] args) {
+
+        BankAccount sallyAccount = new BankAccount();
+
+        sallyAccount.deposit(4.0);
+        System.out.println(sallyAccount.getBalance());
+
+        sallyAccount.withdraw(2.0);
+        System.out.println(sallyAccount.getBalance());
+
+
+//        BankAccount bobAccount = new BankAccount();
+//
+//        sallyAccount.transfer(bobAccount, 2.0);
+//
+//        System.out.println("Transferring 2 from Steve to Bob...");
+//        System.out.println(sallyAccount.getBalance());
+//        System.out.println(bobAccount.getBalance());
+    }
+
+}
